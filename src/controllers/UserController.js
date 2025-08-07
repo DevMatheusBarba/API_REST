07afbe336d99e2dc0fa3cba5cae1b0158d68ddb2
Matheus Silva - Trabalho <matheus.silva081@icloud.com>
2025-08-07 15:50:58 -0300

    Atualizando o CRUD de user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,62 @@ class UserController {
   async create(req, res) {
     try {
       const newUser = await User.create(req.body);
-      res.json(newUser);
+      return res.json(newUser);
     } catch (error) {
-      res.status(400).json({ errors: error.errors.map((e) => e.message) });
+      return res.status(400).json({ errors: error.errors.map((e) => e.message) });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // show
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) return res.status(400).json({ errors: ['ID não informado'] });
+      const user = await User.findByPk(id);
+      if (!user) return res.status(400).json({ errors: ['Usuário não encontrado'] });
+
+      user.update(req.body);
+
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((e) => e.message) });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) return res.status(400).json({ errors: ['ID não informado'] });
+      const user = await User.findByPk(id);
+      if (!user) return res.status(400).json({ errors: ['Usuário não encontrado'] });
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((e) => e.message) });
     }
   }
 }
