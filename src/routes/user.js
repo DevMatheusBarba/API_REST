c01577a2510e882d00350b03e3d33bffe556d361
Matheus Silva - Trabalho <matheus.silva081@icloud.com>
2025-08-08 10:14:41 -0300

    Criacao do JWT

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import loginRequire from '../middlewares/loginRequire';
 
 const router = new Router();
 
 router.post('/', UserController.create);
-router.get('/', UserController.index);
+router.get('/', loginRequire, UserController.index);
 router.get('/:id', UserController.show);
 router.put('/:id', UserController.update);
 router.delete('/:id', UserController.delete);
