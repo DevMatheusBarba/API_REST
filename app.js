a257a519e0d91cc9524058a56d7a2b70c025340a
Matheus Silva - Trabalho <matheus.silva081@icloud.com>
2025-10-02 14:34:49 -0300

    Ajuste de cors

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 import dotenv from 'dotenv';
+import { resolve } from 'path';
 
 dotenv.config();
 
 import './src/database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import home from './src/routes/home';
 import user from './src/routes/user';
 import token from './src/routes/token';
+
+const whiteList = [
+  'https://api-escola.duckdns.org',
+  'localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class APP {
   constructor() {
@@ -17,8 +35,11 @@ class APP {
   }
 
   middlewares() {
+    this.app.use(helmet());
+    this.app.use(cors(corsOptions));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
   routes() {
